b966df10ff3d6ac8e89128c046b5c13971019f55
Prabhat Singh <singhprabhat7.37@gmail.com>
2025-09-19 21:18:28 +0000

    conversion of Datatypes

---
+++ 01_basics/03_conversionOperations.js
@@ -0,0 +1,16 @@
+let Score = "50"
+
+//console.log(typeof (Score))
+
+let valueInNumber=Number(Score)
+//console.log(typeof valueInNumber);
+
+// 33 => 33 in number
+// "33abc" => NaN
+// true => 1 ; False => 0
+
+ let loginStatus=undefined
+
+ let BooleanValue=Boolean(loginStatus)
+
+ console.log(BooleanValue)
