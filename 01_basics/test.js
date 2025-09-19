81b2619cf19556a98c73286ff547445d1d1dd557
Prabhat Singh <singhprabhat7.37@gmail.com>
2025-09-19 20:57:10 +0000

    Datatypes

--- 01_basics/test.js
+++ 01_basics/test.js
@@ -1,1 +1,10 @@
-console.log("Prabha")
+const accountId = 12345;
+let accountName = "Prabhat";
+var accountCountry = "India";
+let accountState;
+
+accountName = "Pooja";
+accountCountry = "USA";
+//Prefer not to use Var because of block scope & Functional scope//
+
+console.table([accountName, accountCountry, accountState]);
