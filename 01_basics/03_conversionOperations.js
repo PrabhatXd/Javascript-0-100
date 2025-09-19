6cbc07f7aed576715957a4ebac6b9307fc119313
Prabhat Singh <singhprabhat7.37@gmail.com>
2025-09-19 21:45:20 +0000

    Operations done and learn prefix and postfix

--- 01_basics/03_conversionOperations.js
+++ 01_basics/03_conversionOperations.js
@@ -13,4 +13,38 @@ let valueInNumber=Number(Score)
 
  let BooleanValue=Boolean(loginStatus)
 
- console.log(BooleanValue)
+ //console.log(BooleanValue)
+
+ // **************************** Operations ****************************************
+
+let value = 3
+let negValue= -value
+console.log(negValue) // operations
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2/3);
+// console.log(2*3);
+// console.log(2**2);
+// console.log(2%3);
+
+let str1 = "Hello"
+let str2 = "Prabhat"
+let str3 = str1 + str2
+console.log(str3)
+
+console.log(1+"2") //output = 12
+console.log("1"+2) //output = 12
+
+console.log("1"+2+2) // output = 122 if the first no is string it will count all the index value as a string
+console.log(1+2+"2"); // output = 32 if its number then it will do the operation and then check the string value
+
+let gameCounter = 10
+gameCounter++    //postfix
+let counterGame = 55
+++counterGame // Prefix
+
+console.log(gameCounter)
+console.log(counterGame);
+
+// Note Google Prefix and Postfix
