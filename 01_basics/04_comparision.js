c60e950e96e6abeffcc2e636fcf32ff4725ad1e6
Prabhat Singh <singhprabhat7.37@gmail.com>
2025-09-20 10:21:04 +0000

    Datatypes Summary

---
+++ 01_basics/04_comparision.js
@@ -0,0 +1,13 @@
+// console.log(2>1)
+// console.log(2>=1)
+// console.log(2==2)
+
+// console.log(null>0) // false
+// console.log(null==0) // false 
+// console.log(null >=0); // true because of equality check "==" and <> comparision value works differently
+
+
+console.log("2"==2) // this returns true bcoz while equality check the string get converts into no thus its giving true 
+console.log("2"===2) // while conversion this will check value and also the data type , "2" is string and 2 is number both are different data types
+
+
